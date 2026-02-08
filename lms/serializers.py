4de0fcb8f428772bae8a557121f492f3d91c609d
Ирина Судейкина <irinaasster@gmail.com>
2026-02-08 17:55:11 +0500

    view courses

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from lms.models import Lesson, Course
+from users.models import Subscription
 from lms.validators import validate_url_source
 
 
@@ -14,10 +15,16 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lesson = serializers.SerializerMethodField()
+    is_sub = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
 
     def get_count_lesson(self, course):
         return course.lessons.count()
+
+    def get_is_sub(self, course):
+        """Проверяет, подписан ли текущий пользователь на курс."""
+        user = self.context['request'].user
+        return Subscription.objects.filter(user=user, course=course).exists()
 
     class Meta:
         model = Course
